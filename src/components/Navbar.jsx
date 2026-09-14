a784a981a09560cb5270884baca5ba56847cf6a1
Tomas-zuleta <tomaszuleta33@gmail.com>
2026-09-14 06:52:07 -0500

    changes in index.css and crete logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,22 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <header>
-      <h1>La Bodega del Juguete</h1>
-      <nav>
+    <header className="navbar">
+
+      {/* LOGO */}
+      <div className="logo">
+        <img src="/logo.png" alt="logo" />
+        <span>Anime Store</span>
+      </div>
+
+      {/* LINKS */}
+      <nav className="nav-links">
         <Link to="/">Inicio</Link>
         <Link to="/nosotros">Nosotros</Link>
         <Link to="/productos">Productos</Link>
         <Link to="/contacto">Contacto</Link>
       </nav>
+
     </header>
   );
 }
